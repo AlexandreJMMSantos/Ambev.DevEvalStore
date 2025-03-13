5c4cab592a14144d8f646f7186db03530eeb188a
AlexandreJMMSantos <alexandretcq@gmail.com>
2025-03-12 21:37:28 -0300

    Ajuste de mapping

--- src/Ambev.DeveloperEvaluation.WebApi/Features/Auth/AuthenticateUserFeature/AuthenticateUserProfile.cs
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Auth/AuthenticateUserFeature/AuthenticateUserProfile.cs
@@ -15,10 +15,10 @@ public sealed class AuthenticateUserProfile : Profile
     public AuthenticateUserProfile()
     {
         CreateMap<User, AuthenticateUserResponse>()
-            .ForMember(dest => dest.Token, opt => opt.Ignore())
+            .ForMember(dest => dest.Token, opt => opt.Ignore())  
             .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role.ToString()));
 
         CreateMap<AuthenticateUserRequest, AuthenticateUserCommand>();
-        CreateMap<AuthenticateUserResponse, AuthenticateUserCommand>();
+        CreateMap<AuthenticateUserResult, AuthenticateUserResponse>();
     }
 }
